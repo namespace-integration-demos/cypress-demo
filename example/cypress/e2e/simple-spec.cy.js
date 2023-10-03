79bea5f9d39b38a85e19746cdf95a069ee7d8201
Niklas Gehlen <niklas@namespacelabs.com>
2023-10-03 11:39:22 +0200

    Uplift cookie, fix regex

--- example/cypress/e2e/simple-spec.cy.js
+++ example/cypress/e2e/simple-spec.cy.js
@@ -1,16 +1,18 @@
 /// <reference types="cypress" />
+beforeEach(() => {
+  const token = Cypress.env('token');
+  const workspace = Cypress.env('workspace');
+  cy.setCookie(`nsc-auth-token-tenant-${ workspace }`, token);
+})
+
 describe('Example config', () => {
   it('has baseUrl', () => {
-    expect(Cypress.config('baseUrl')).to.match(
-      '/\.namespaced\.app$/'
+    expect(Cypress.config('baseUrl'), 'URL is a Namespace preview').to.match(
+      /\.namespaced\.app$/
     )
   })
 
   it('loads the page', () => {
-    const token = Cypress.env('token');
-    const workspace = Cypress.env('workspace');
-    cy.setCookie(`nsc-auth-token-tenant-${ workspace }`, token);
-
     cy.visit('/')
     cy.contains('Welcome to nginx!').should('be.visible')
   })
