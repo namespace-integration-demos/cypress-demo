49a95806dbcb48972ef064fa5d41bdf8999aad20
Niklas Gehlen <niklas@namespacelabs.com>
2023-10-03 16:30:24 +0200

    Set up example. (#1)

---
+++ example/cypress/e2e/simple-spec.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+beforeEach(() => {
+  cy.intercept(`${Cypress.config('baseUrl')}**`, req => {
+      req.headers['x-nsc-ingress-auth'] = `Bearer ${ Cypress.env('token') }`
+  })
+})
+
+describe('Example config', () => {
+  it('has baseUrl', () => {
+    expect(Cypress.config('baseUrl'), 'URL is a Namespace preview').to.match(
+      /\.namespaced\.app$/
+    )
+  })
+
+  it('loads the page', () => {
+    cy.visit('/')
+    cy.contains('Welcome to nginx!').should('be.visible')
+  })
+
+  it.skip('fails on purpose', () => {
+    // to verify that the action exits correctly
+    expect(false).to.be.true
+  })
+})
