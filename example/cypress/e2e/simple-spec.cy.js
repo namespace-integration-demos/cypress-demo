36458e70cbcd0f963854b77db19c39105fe7f3c1
Niklas Gehlen <niklas@namespacelabs.com>
2023-10-03 10:31:18 +0200

    Inject bearer token into cypress

--- example/cypress/e2e/simple-spec.cy.js
+++ example/cypress/e2e/simple-spec.cy.js
@@ -2,12 +2,20 @@
 describe('Example config', () => {
   it('has baseUrl', () => {
     expect(Cypress.config('baseUrl')).to.match(
-      '\.namespaced\.app$'
+      '/\.namespaced\.app$/'
     )
   })
 
+  const token = Cypress.env('token');
+  const authorization = `bearer ${ token }`;
+  const options = {
+    method: 'GET',
+    headers: {
+      authorization,
+    }};
+
   it('loads the page', () => {
-    cy.visit('/')
+    cy.visit('/', options)
     cy.contains('Welcome to nginx!').should('be.visible')
   })
 
